8b030adc4d0a81cc75d56fa69a11722d9f1af223
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-09-26 15:24:07 +0200

    fixing logger issues

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
 
+export interface UserDocument extends mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
@@ -12,5 +20,7 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async (next) => {});
 
 export const UserModel = mongoose.model("User", userSchema);
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,9 @@ import logger from "pino";
 import dayjs from "dayjs";
 
 export const log = logger({
+  transport: {
+    target: "pino-pretty",
+  },
   base: {
     pid: false,
   },
