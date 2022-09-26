e62fbfe19b3ff26c35f426e1c5037e4fe13c53a4
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-09-26 16:46:34 +0200

    omitting password when creating user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { omit } from "lodash";
 import { CreateUserInput } from "../schemas/user.schema";
 import { createUser } from "../services/user.service";
 import { log } from "../utils/logger";
@@ -9,7 +10,7 @@ export const createUserHandler = async (
 ) => {
   try {
     const user = await createUser(req.body);
-    return user;
+    return res.send(omit(user.toJSON(), "password"));
   } catch (error: any) {
     log.error(error);
     /* 409 = conflict - if function throws it has violated unique restrictions*/
