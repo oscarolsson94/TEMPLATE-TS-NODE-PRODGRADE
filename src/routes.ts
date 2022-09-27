cbcf8a8f42b4494b48a24e6ce0f7caa3f3df3e2b
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-09-27 10:47:02 +0200

    added session route and validating schema

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Express, Request, Response } from "express";
+import { createSessionHandler } from "./controllers/session.controller";
 import { createUserHandler } from "./controllers/user.controller";
-import { validate } from "./middleware/validateResource";
+import { validateResource } from "./middleware/validateResource";
+import { createSessionSchema } from "./schemas/session.schema";
 import { createUserSchema } from "./schemas/user.schema";
 
 export const routes = (app: Express) => {
@@ -8,5 +10,11 @@ export const routes = (app: Express) => {
     res.sendStatus(200)
   );
 
-  app.post("/api/users", validate(createUserSchema), createUserHandler);
+  app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+
+  app.post(
+    "/api/sessions",
+    validateResource(createSessionSchema),
+    createSessionHandler
+  );
 };
