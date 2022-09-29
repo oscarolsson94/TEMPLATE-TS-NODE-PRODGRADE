5ff229656b060f7185e737be3d756f3960cd3c2a
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-09-29 11:41:51 +0200

    added product routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Express, Request, Response } from "express";
 import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from "./controllers/product.controller";
+import {
   createSessionHandler,
   deleteSessionHandler,
   getUserSessionsHandler,
@@ -7,6 +13,12 @@ import {
 import { createUserHandler } from "./controllers/user.controller";
 import { requireUser } from "./middleware/requireUser";
 import { validateResource } from "./middleware/validateResource";
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema,
+} from "./schemas/product.schema";
 import { createSessionSchema } from "./schemas/session.schema";
 import { createUserSchema } from "./schemas/user.schema";
 
@@ -26,4 +38,50 @@ export const routes = (app: Express) => {
   app.get("/api/sessions", requireUser, getUserSessionsHandler);
 
   app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+  app.post(
+    "/api/products",
+    [requireUser, validateResource(createProductSchema)],
+    createProductHandler
+  );
+
+  /**
+   * @openapi
+   * '/api/products/{productId}':
+   *  get:
+   *     tags:
+   *     - Products
+   *     summary: Get a single product by the productId
+   *     parameters:
+   *      - name: productId
+   *        in: path
+   *        description: The id of the product
+   *        required: true
+   *     responses:
+   *       200:
+   *         description: Success
+   *         content:
+   *          application/json:
+   *           schema:
+   *              $ref: '#/components/schema/Product'
+   *       404:
+   *         description: Product not found
+   */
+  app.put(
+    "/api/products/:productId",
+    [requireUser, validateResource(updateProductSchema)],
+    updateProductHandler
+  );
+
+  app.get(
+    "/api/products/:productId",
+    validateResource(getProductSchema),
+    getProductHandler
+  );
+
+  app.delete(
+    "/api/products/:productId",
+    [requireUser, validateResource(deleteProductSchema)],
+    deleteProductHandler
+  );
 };
